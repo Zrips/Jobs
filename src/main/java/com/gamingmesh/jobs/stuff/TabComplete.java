925ee2c4789d30196ca91c1bbe3440f4c8773e60
montlikadani <montlikada@gmail.com>
2018-08-16 17:54:30 +0200

    Update TabComplete.java

--- src/main/java/com/gamingmesh/jobs/stuff/TabComplete.java
+++ src/main/java/com/gamingmesh/jobs/stuff/TabComplete.java
@@ -28,7 +28,7 @@ public class TabComplete implements TabCompleter {
 
 	if (args.length == 1) {
 	    String PartOfCommand = args[0];
-	    List<String> temp = new ArrayList<String>();
+	    List<String> temp = new ArrayList<>();
 
 	    for (Entry<String, Integer> BCmd : Jobs.getCommandManager().GetCommands(sender).entrySet()) {
 		temp.add(BCmd.getKey());
@@ -50,7 +50,7 @@ public class TabComplete implements TabCompleter {
 			continue;
 
 		    String arg = ArgsList.get(i - 1);
-		    List<String> temp = new ArrayList<String>();
+		    List<String> temp = new ArrayList<>();
 
 		    if (arg.contains("%%"))
 			for (String one : arg.split("%%")) {
