3308a5236b171656956b61823a110bad345b15b6
montlikadani <montlikada@gmail.com>
2018-08-16 18:29:18 +0200

    Update ExploreManager.java

--- src/main/java/com/gamingmesh/jobs/config/ExploreManager.java
+++ src/main/java/com/gamingmesh/jobs/config/ExploreManager.java
@@ -22,7 +22,7 @@ import com.gamingmesh.jobs.stuff.Debug;
 
 public class ExploreManager {
 
-    private HashMap<String, ExploreRegion> worlds = new HashMap<String, ExploreRegion>();
+    private HashMap<String, ExploreRegion> worlds = new HashMap<>();
     private boolean exploreEnabled = false;
     private int playerAmount = 1;
 
