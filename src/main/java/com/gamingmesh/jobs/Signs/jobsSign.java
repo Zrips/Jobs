355074dffdb165437b5b9215ebe895d1edc74609
montlikadani <montlikada@gmail.com>
2019-08-14 14:09:32 +0200

    We don't need the x, y, z methods, it can be get from location

--- src/main/java/com/gamingmesh/jobs/Signs/jobsSign.java
+++ src/main/java/com/gamingmesh/jobs/Signs/jobsSign.java
@@ -44,30 +44,6 @@ public class jobsSign {
 	return worldName;
     }
 
-    public void setX(int x) {
-	this.x = x;
-    }
-
-    public double getX() {
-	return x;
-    }
-
-    public void setY(int y) {
-	this.y = y;
-    }
-
-    public double getY() {
-	return y;
-    }
-
-    public void setZ(int z) {
-	this.z = z;
-    }
-
-    public double getZ() {
-	return z;
-    }
-
     public Location getLocation() {
 	if (loc != null)
 	    return loc;
--- src/main/java/com/gamingmesh/jobs/stuff/blockLoc.java
+++ src/main/java/com/gamingmesh/jobs/stuff/blockLoc.java
@@ -24,30 +24,6 @@ public class blockLoc {
 	worldName = loc.getWorld().getName();
     }
 
-    public int getX() {
-	return x;
-    }
-
-    public void setX(int x) {
-	this.x = x;
-    }
-
-    public int getY() {
-	return y;
-    }
-
-    public void setY(int y) {
-	this.y = y;
-    }
-
-    public int getZ() {
-	return z;
-    }
-
-    public void setZ(int z) {
-	this.z = z;
-    }
-
     public String getWorldName() {
 	return worldName;
     }
