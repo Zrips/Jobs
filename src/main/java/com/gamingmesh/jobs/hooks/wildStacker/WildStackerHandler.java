dd955ec79e298d9f4885923ca6b5d350b3651326
Zrips <Zripas@gmail.com>
2021-11-29 16:47:44 +0200

    WildStacker fix

--- src/main/java/com/gamingmesh/jobs/hooks/wildStacker/WildStackerHandler.java
+++ src/main/java/com/gamingmesh/jobs/hooks/wildStacker/WildStackerHandler.java
@@ -3,17 +3,20 @@ package com.gamingmesh.jobs.hooks.wildStacker;
 import java.util.List;
 
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 import com.bgsoftware.wildstacker.api.WildStackerAPI;
 import com.bgsoftware.wildstacker.api.objects.StackedEntity;
 
 public class WildStackerHandler {
 
-	public boolean isStackedEntity(LivingEntity entity) {
-		return WildStackerAPI.getStackedEntity(entity) != null;
-	}
+    public boolean isStackedEntity(LivingEntity entity) {
+	if (entity instanceof Player)
+	    return false;
+	return WildStackerAPI.getStackedEntity(entity) != null;
+    }
 
-	public List<StackedEntity> getStackedEntities() {
-		return WildStackerAPI.getWildStacker().getSystemManager().getStackedEntities();
-	}
+    public List<StackedEntity> getStackedEntities() {
+	return WildStackerAPI.getWildStacker().getSystemManager().getStackedEntities();
+    }
 }
