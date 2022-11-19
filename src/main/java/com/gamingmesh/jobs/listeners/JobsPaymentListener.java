3425a3ba1ab5966a456ed32cad32d23604cbf563
smuddgge <68658429+smuddgge@users.noreply.github.com>
2022-11-19 13:26:34 +0000

    Reverted fishing exploiting

--- src/main/java/com/gamingmesh/jobs/listeners/JobsPaymentListener.java
+++ src/main/java/com/gamingmesh/jobs/listeners/JobsPaymentListener.java
@@ -543,7 +543,7 @@ public final class JobsPaymentListener implements Listener {
 
                 // check is the fishing being exploited. If yes, prevent payment.
                 if (mcMMOPlayer != null && ExperienceConfig.getInstance().isFishingExploitingPrevented()
-                    && mcMMOPlayer.getFishingManager().isFishingTooOften()) {
+                    && mcMMOPlayer.getFishingManager().isExploitingFishing(event.getHook().getLocation().toVector())) {
                     return;
                 }
             }
