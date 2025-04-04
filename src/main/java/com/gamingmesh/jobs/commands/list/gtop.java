aaaeb5b1dfdb8cda1a9b365e3b47eeb3b94202d2
Zrips <zripas@gmail.com>
2025-04-04 12:11:52 +0300

    gtop and top command cleanup

--- src/main/java/com/gamingmesh/jobs/commands/list/gtop.java
+++ src/main/java/com/gamingmesh/jobs/commands/list/gtop.java
@@ -1,7 +1,6 @@
 package com.gamingmesh.jobs.commands.list;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
@@ -14,8 +13,8 @@ import com.gamingmesh.jobs.container.JobsPlayer;
 import com.gamingmesh.jobs.container.TopList;
 import com.gamingmesh.jobs.i18n.Language;
 
+import net.Zrips.CMILib.Container.CMIList;
 import net.Zrips.CMILib.Container.PageInfo;
-import net.Zrips.CMILib.Locale.LC;
 import net.Zrips.CMILib.Messages.CMIMessages;
 import net.Zrips.CMILib.Scoreboards.CMIScoreboard;
 import net.Zrips.CMILib.Version.Schedulers.CMIScheduler;
@@ -24,29 +23,30 @@ public class gtop implements Cmd {
 
     @Override
     public Boolean perform(Jobs plugin, final CommandSender sender, final String[] args) {
-        if (!(sender instanceof Player)) {
-            CMIMessages.sendMessage(sender, LC.info_Ingame);
-            return null;
-        }
 
         if (args.length > 1) {
             return false;
         }
 
-        Player player = (Player) sender;
+        Player player = sender instanceof Player ? (Player) sender : null;
+
         int page = 1;
-        if (args.length == 1) {
-            if (args[0].equalsIgnoreCase("clear")) {
+
+        for (String one : args) {
+            if (one.equalsIgnoreCase("clear")) {
+                if (player == null)
+                    return false;
                 player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
                 CMIScoreboard.removeScoreBoard(player);
                 return true;
             }
 
-            try {
-                page = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                return true;
-            }
+            if (page < 1)
+                try {
+                    page = Integer.parseInt(args[1]);
+                    continue;
+                } catch (NumberFormatException e) {
+                }
         }
 
         if (page < 1)
@@ -59,7 +59,6 @@ public class gtop implements Cmd {
     }
 
     private static void showGlobalTop(CommandSender sender, int page) {
-        Player player = (Player) sender;
 
         int amount = Jobs.getGCManager().JobsTopAmount;
 
@@ -72,64 +71,44 @@ public class gtop implements Cmd {
             return;
         }
 
-        List<TopList> list = getSafeSubList(fullList, pi.getStart(), pi.getEnd());
+        List<String> ls = new ArrayList<>();
 
-        if (!Jobs.getGCManager().ShowToplistInScoreboard) {
-            Language.sendMessage(sender, "command.gtop.output.topline", "%amount%", amount);
+        for (int i = 0; i < amount; i++) {
 
-            int i = 0;
-            for (TopList one : list) {
+            if (fullList.size() <= i + pi.getStart())
+                break;
 
-                if (i >= amount)
-                    break;
+            TopList one = fullList.get(i + pi.getStart());
 
-                JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
+            JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
 
-                if (jPlayer != null)
-                    Language.sendMessage(sender, "command.gtop.output.list",
-                        "%number%", pi.getPositionForOutput(i),
-                        "%playername%", jPlayer.getName(),
-                        "%playerdisplayname%", jPlayer.getDisplayName(),
-                        "%level%", one.getLevel(),
-                        "%exp%", one.getExp());
-                ++i;
-            }
+            if (jPlayer == null)
+                continue;
+
+            if (Jobs.getGCManager().ShowToplistInScoreboard && sender instanceof Player)
+                ls.add(Jobs.getLanguage().getMessage("scoreboard.line",
+                    "%number%", pi.getPositionForOutput(i),
+                    "%playername%", jPlayer.getName(),
+                    "%playerdisplayname%", jPlayer.getDisplayName(),
+                    "%level%", one.getLevel(),
+                    "%exp%", one.getExp()));
+            else
+                ls.add(Jobs.getLanguage().getMessage("command.gtop.output.list",
+                    "%number%", pi.getPositionForOutput(i),
+                    "%playername%", jPlayer.getName(),
+                    "%playerdisplayname%", jPlayer.getDisplayName(),
+                    "%level%", one.getLevel(),
+                    "%exp%", one.getExp()));
+        }
+
+        if (Jobs.getGCManager().ShowToplistInScoreboard && sender instanceof Player) {
+            CMIScoreboard.show((Player) sender, Jobs.getLanguage().getMessage("scoreboard.gtopline"), ls, Jobs.getGCManager().ToplistInScoreboardInterval);
         } else {
-            List<String> ls = new ArrayList<>();
-            int i = 0;
-            for (TopList one : list) {
-                if (i >= amount)
-                    break;
-
-                JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
-
-                if (jPlayer != null)
-                    ls.add(Jobs.getLanguage().getMessage("scoreboard.line",
-                        "%number%", pi.getPositionForOutput(i),
-                        "%playername%", jPlayer.getName(),
-                        "%playerdisplayname%", jPlayer.getDisplayName(),
-                        "%level%", one.getLevel()));
-                ++i;
-            }
-
-            CMIScoreboard.show(player, Jobs.getLanguage().getMessage("scoreboard.gtopline"), ls, Jobs.getGCManager().ToplistInScoreboardInterval);
+            Language.sendMessage(sender, "command.gtop.output.topline", "%amount%", amount);
+            CMIMessages.sendMessage(sender, CMIList.listToString(ls));
         }
 
         pi.autoPagination(sender, "jobs gtop");
     }
 
-    public static List<TopList> getSafeSubList(List<TopList> list, int fromIndex, int toIndex) {
-        int size = list.size();
-        toIndex++;
-        if (fromIndex >= size)
-            return Collections.emptyList();
-        if (toIndex > size)
-            toIndex = size;
-        if (fromIndex < 0)
-            fromIndex = 0;
-        if (fromIndex >= toIndex)
-            return Collections.emptyList();
-
-        return new ArrayList<>(list.subList(fromIndex, toIndex));
-    }
 }
--- src/main/java/com/gamingmesh/jobs/commands/list/top.java
+++ src/main/java/com/gamingmesh/jobs/commands/list/top.java
@@ -1,26 +1,25 @@
 package com.gamingmesh.jobs.commands.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+
 import com.gamingmesh.jobs.Jobs;
 import com.gamingmesh.jobs.commands.Cmd;
 import com.gamingmesh.jobs.container.Job;
 import com.gamingmesh.jobs.container.JobsPlayer;
 import com.gamingmesh.jobs.container.TopList;
 import com.gamingmesh.jobs.i18n.Language;
+
+import net.Zrips.CMILib.Container.CMIList;
 import net.Zrips.CMILib.Container.PageInfo;
 import net.Zrips.CMILib.Locale.LC;
 import net.Zrips.CMILib.Messages.CMIMessages;
 import net.Zrips.CMILib.Scoreboards.CMIScoreboard;
 import net.Zrips.CMILib.Version.Schedulers.CMIScheduler;
-
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class top implements Cmd {
 
@@ -33,83 +32,96 @@ public class top implements Cmd {
 
         Player player = sender instanceof Player ? (Player) sender : null;
 
-        if (args[0].equalsIgnoreCase("clear")) {
-            if (player != null) {
+        int page = 0;
+        Job job = null;
+
+        for (String one : args) {
+            if (one.equalsIgnoreCase("clear")) {
+                if (player == null)
+                    return false;
+
                 player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
                 CMIScoreboard.removeScoreBoard(player);
+
+                return true;
             }
-            return true;
+
+            if (job == null) {
+                job = Jobs.getJob(one);
+                if (job != null)
+                    continue;
+            }
+            if (page < 1)
+                try {
+                    page = Integer.parseInt(args[1]);
+                    continue;
+                } catch (NumberFormatException e) {
+                }
         }
 
-        int page = 1;
-        if (args.length == 2) {
-            try {
-                page = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-        if (page < 1)
-            page = 1;
-
-        Job job = Jobs.getJob(args[0]);
         if (job == null) {
             Language.sendMessage(sender, "command.top.error.nojob");
             return null;
         }
 
+        if (page < 1)
+            page = 1;
+
         final int finalPage = page;
-        CMIScheduler.runTaskAsynchronously(plugin, () -> showTop(sender, job, finalPage));
+        final Job finalJob = job;
+        CMIScheduler.runTaskAsynchronously(plugin, () -> showTop(sender, finalJob, finalPage));
         return true;
     }
 
     private static void showTop(CommandSender sender, Job job, int page) {
-        Player player = (Player) sender;
+
         List<TopList> fullList = Jobs.getJobsDAO().toplist(job.getName());
 
         if (fullList.isEmpty()) {
             CMIMessages.sendMessage(sender, LC.info_NoInformation);
             return;
         }
+        int amount = Jobs.getGCManager().JobsTopAmount;
+        PageInfo pi = new PageInfo(amount, fullList.size(), page);
 
-        PageInfo pi = new PageInfo(Jobs.getGCManager().JobsTopAmount, fullList.size(), page);
+        List<String> ls = new ArrayList<>();
 
-        List<TopList> list = gtop.getSafeSubList(fullList, pi.getStart(), pi.getEnd());
+        for (int i = 0; i < amount; i++) {
 
-        int place = 1;
+            if (fullList.size() <= i + pi.getStart())
+                break;
 
-        if (!Jobs.getGCManager().ShowToplistInScoreboard || player == null) {
-            Language.sendMessage(sender, "command.top.output.topline", job, "%amount%", Jobs.getGCManager().JobsTopAmount);
+            TopList one = fullList.get(i + pi.getStart());
 
-            for (TopList one : list) {
+            JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
 
-                JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
+            if (jPlayer == null)
+                continue;
 
-                Language.sendMessage(sender, "command.top.output.list",
-                    "%number%", ((page - 1) * Jobs.getGCManager().JobsTopAmount) + place,
+            if (Jobs.getGCManager().ShowToplistInScoreboard && sender instanceof Player)
+                ls.add(Jobs.getLanguage().getMessage("scoreboard.line",
+                    "%number%", pi.getPositionForOutput(i),
                     "%playername%", jPlayer.getName(),
                     "%playerdisplayname%", jPlayer.getDisplayName(),
                     "%level%", one.getLevel(),
-                    "%exp%", one.getExp());
-                place++;
-            }
-            pi.autoPagination(sender, "jobs top " + job.getName());
-        } else {
-            List<String> ls = new ArrayList<>();
-
-            for (TopList one : list) {
-
-                JobsPlayer jPlayer = Jobs.getPlayerManager().getJobsPlayer(one.getUuid());
-
-                ls.add(Jobs.getLanguage().getMessage("scoreboard.line", "%number%", ((page - 1) * Jobs.getGCManager().JobsTopAmount) + place,
-                    "%playername%", jPlayer.getName(), "%playerdisplayname%", jPlayer.getDisplayName(), "%level%", one.getLevel()));
-                place++;
-            }
-
-            CMIScoreboard.show(player, Jobs.getLanguage().getMessage("scoreboard.topline", job), ls, Jobs.getGCManager().ToplistInScoreboardInterval);
-
-            pi.autoPagination(sender, "jobs top " + job.getName());
+                    "%exp%", one.getExp()));
+            else
+                ls.add(Jobs.getLanguage().getMessage("command.top.output.list",
+                    "%number%", pi.getPositionForOutput(i),
+                    "%playername%", jPlayer.getName(),
+                    "%playerdisplayname%", jPlayer.getDisplayName(),
+                    "%level%", one.getLevel(),
+                    "%exp%", one.getExp()));
         }
+
+        if (Jobs.getGCManager().ShowToplistInScoreboard && sender instanceof Player) {
+            CMIScoreboard.show((Player) sender, Jobs.getLanguage().getMessage("scoreboard.topline", job), ls, Jobs.getGCManager().ToplistInScoreboardInterval);
+        } else {
+            Language.sendMessage(sender, "command.top.output.topline", job, "%amount%", Jobs.getGCManager().JobsTopAmount);
+            CMIMessages.sendMessage(sender, CMIList.listToString(ls));
+        }
+
+        pi.autoPagination(sender, "jobs top " + job.getName());
     }
 
 }
